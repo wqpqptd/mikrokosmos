5ef9ae94eb2036da9b9f0af6204de073a3944cb5
wqpqptd <bmdi1013@gmail.com>
2022-04-05 22:08:43 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
@@ -15,4 +20,3 @@ const router = createRouter({
 });
 
 export default router;
-
